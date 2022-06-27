8b5d22b03302d52a1c714e56af97c55b55fc452f
Elizabeth <105356867+Lisaojet@users.noreply.github.com>
2022-06-27 10:45:31 +0100

    Update models.py

--- I4G020346G2H/blog/models.py
+++ I4G020346G2H/blog/models.py
@@ -6,10 +6,10 @@ User = get_user_model()
 # Create your models here.
 class Post(models.Model):
     Title = models.CharField(max_length=200)
-    Text = models.TextField
+    Text = models.TextField()
     Author = models.ForeignKey(User, on_delete=models.CASCADE)
-    Created_date = models.DateTimeField
-    Published_date = models.DateTimeField
+    Created_date = models.DateTimeField()
+    Published_date = models.DateTimeField()
 
     def __str__(self) -> str:
         return self.title
